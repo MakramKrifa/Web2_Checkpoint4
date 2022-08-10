0b77b4e73b003530db17b07d757a07af612914bb
Makram Krifa <108370654+MakramKrifa@users.noreply.github.com>
2022-08-10 12:48:27 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Form_class'>
+      <h1 className='Title'>Contact Us</h1>
+    <Form >
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control className='Input_class' type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control className='Input_class' type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <div className='Btn-div'>
+        <Button className='Btn-submit' variant="primary" type="submit">
+          Submit
+        </Button>
+      </div>
+    </Form>
     </div>
   );
 }
---
+++ src/AppClass.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./App.css"
+
+class AppClass extends React.Component {
+    test(event) {
+      document.getElementById("hmed").type = "password"
+    }
+    render() {
+        return (
+          <div className='Form_class'>
+          <h1 className='Title'>Contact Us</h1><Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control className='Input_class' type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control className='Input_class' type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <div className='Btn-div'>
+                <Button className='Btn-submit' variant="primary" type="submit">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+        </div>
+        )
+    }
+  }
+    export default AppClass;
